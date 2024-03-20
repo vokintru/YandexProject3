6e5d9ae4c9e290d4ac4b4b1d4e5ac61efbb72ce0
vokintru <aodplayer@bk.ru>
2024-03-20 14:38:34 +0300

    Добавлен логотип, файлы на будущее и недоделанная система заполнения профиля

--- server/data/users.py
+++ server/data/users.py
@@ -1,4 +1,6 @@
 import datetime
+import random
+
 import sqlalchemy
 from flask_login import UserMixin
 
@@ -18,10 +20,39 @@ class User(SqlAlchemyBase, UserMixin):
                                       default=datetime.datetime.now)
 
     def __repr__(self):
-        return f'<User> {self.id} {self.surname} {self.name}'
+        return f'<User> {self.id} {self.username} {self.modified_date}'
 
     def set_password(self, password):
         self.hashed_password = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.hashed_password, password)
+
+
+class Account(SqlAlchemyBase, UserMixin):
+    __tablename__ = 'accounts'
+
+    default_avatars = [
+        "static/img/default_avatars/avatar0.png",
+        "static/img/default_avatars/avatar1.png",
+        "static/img/default_avatars/avatar2.png",
+        "static/img/default_avatars/avatar3.png",
+        "static/img/default_avatars/avatar4.png",
+        "static/img/default_avatars/avatar5.png",
+        "static/img/default_avatars/avatar6.png",
+        "static/img/default_avatars/avatar7.png",
+        "static/img/default_avatars/avatar8.png",
+        "static/img/default_avatars/avatar9.png",
+    ]
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    bio = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    avatar = sqlalchemy.Column(sqlalchemy.String, nullable=False, default=random.choice(default_avatars))
+    followers = sqlalchemy.Column(JSON, nullable=True, default=[])
+    follow = sqlalchemy.Column(JSON, nullable=True, default=[])
+
+    def __repr__(self):
+        return f'<User> {self.id} {self.username} {self.name} {self.avatar} {self.followers} {self.follow}'
+
--- server/main.py
+++ server/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect
 from data import db_session
-from data.users import User
+from data.users import User, Account
 from forms.user import RegisterForm, LoginForm
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 
@@ -42,12 +42,12 @@ def reqister():
     form = RegisterForm()
     if form.validate_on_submit():
         if form.password.data != form.password_again.data:
-            return render_template('register.html', title='Регистрация',
+            return render_template('register1.html', title='Регистрация',
                                    form=form,
                                    message="Пароли не совпадают")
         db_sess = db_session.create_session()
         if db_sess.query(User).filter(User.username == form.username.data).first():
-            return render_template('register.html', title='Регистрация',
+            return render_template('register1.html', title='Регистрация',
                                    form=form,
                                    message="Такой пользователь уже есть")
         user = User(
@@ -58,7 +58,24 @@ def reqister():
         db_sess.add(user)
         db_sess.commit()
         return redirect('/')
-    return render_template('register.html', title='Регистрация', form=form)
+    return render_template('register1.html', title='Регистрация', form=form)
+
+
+@app.route('/register_complement', methods=['GET', 'POST'])
+@login_required
+def register_complement():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = User(
+            username=form.username.data,
+            hashed_password=form.password.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/')
+    return render_template('register2.html', title='Регистрация', form=form)
 
 
 @app.route('/logout')
