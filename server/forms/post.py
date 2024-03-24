20be0f4f0f1c298fc8d316a138ff51fc88d39d1f
Nikiton-prog <n.s.ivannikov@gmail.com>
2024-03-24 23:31:56 +0300

    кваканье теперь TextAreaField

--- server/forms/post.py
+++ server/forms/post.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import TextAreaField, SubmitField
 from wtforms.validators import DataRequired
 
 
 class NewPostForm(FlaskForm):
-    text = StringField('Квакание', validators=[DataRequired()])
+    text = TextAreaField('Квакание', validators=[DataRequired()])
     submit = SubmitField('Квакнуть')
 
